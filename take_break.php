3a754e25fde38fe8bd630bf9f38070c074de9cf5
sahalux111 <sahalal77@gmail.com>
2024-09-04 17:41:55 +0530

    take_break.php

---
+++ take_break.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+require '../config/db.php';
+
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'doctor') {
+    header('Location: index.php');
+    exit();
+}
+
+$username = $_SESSION['username'];
+$break_duration = $_POST['break_duration'] ?? 0;
+
+if ($break_duration > 0) {
+    $break_end = (new DateTime('now', new DateTimeZone('Asia/Kolkata')))
+        ->add(new DateInterval("PT{$break_duration}M"));
+    $doctor_breaks[$username] = $break_end;
+    header('Location: dashboard.php');
+    exit();
+} else {
+    echo 'Invalid break duration';
+}
+?>
